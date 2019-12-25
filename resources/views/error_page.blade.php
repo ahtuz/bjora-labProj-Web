5b73820fccfc31f2213707c35f9d56d164d086f8
Ahtuz <albertusheronius@gmail.com>
2019-12-25 12:45:51 +0700

    updated error page

--- resources/views/error_page.blade.php
+++ resources/views/error_page.blade.php
@@ -5,5 +5,8 @@
     <h1 style="left:32%;top:65%;position:absolute;font-size:80px">
         <p class="text-white bg-dark font-italic">Are You Lost?</p>
     </h1>
-    <a href="/" class="btn btn-outline-light btn-lg " role="button" aria-disabled="true" style="left:45%;top:80%;position:absolute">Back to Home</a>
+    <h5 style="left:32%;top:80%;position:absolute;">
+        <p class="text-white bg-dark font-italic">Maybe you should ask more questions...</p>
+    </h5>
+    <a href="/" class="btn btn-outline-light btn-lg " role="button" aria-disabled="true" style="left:42%;top:86%;position:absolute">Lead Me to the Right Way</a>
 @endsection
